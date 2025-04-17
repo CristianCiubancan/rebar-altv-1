2418edfbb9f49884a99b6963275f1eb4af8a23aa
CristianCiubancan <balooprogames@gmail.com>
2025-04-17 05:01:53 +0300

    removed-unused

--- src/plugins/example-plugin/client/index.ts
+++
@@ -1,3 +0,0 @@
-import * as alt from 'alt-client';
-
-alt.log('Example Plugin (Client): Loaded');
--- src/plugins/example-plugin/server/index.ts
+++
@@ -1,5 +0,0 @@
-import * as alt from 'alt-server';
-
-alt.on('playerConnect', (player) => {
-    alt.log('Example Plugin (Server): Player connected');
-});
