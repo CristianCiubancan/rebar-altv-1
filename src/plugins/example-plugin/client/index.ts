06cbce6575632934df91ef41f14dc5d1ad4ba5f1
CristianCiubancan <balooprogames@gmail.com>
2025-04-17 06:44:44 +0300

    fix

--- src/plugins/example-plugin/client/index.ts
+++ src/plugins/example-plugin/client/index.ts
@@ -1,4 +1,32 @@
 import * as alt from 'alt-client';
+import { useWebview } from '@Client/webview/index.js';
+
+const webview = useWebview() as any;
+let uiVisible = false;
+
+function toggleUI() {
+    if (uiVisible) {
+        webview.hide('example-plugin');
+        uiVisible = false;
+        alt.log('Example UI hidden');
+    } else {
+        webview.show('example-plugin', 'page', true);
+        uiVisible = true;
+        alt.log('Example UI shown');
+    }
+}
+
+alt.on('consoleCommand', (cmd) => {
+    if (cmd === 'toggleexample') {
+        toggleUI();
+    }
+});
+
+alt.on('keyup', (key) => {
+    if (key === 0x49) {
+        toggleUI();
+    }
+});
 
 alt.log('Example client plugin loaded successfully!');
 
---
+++ src/types/altv.d.ts
@@ -0,0 +1,16 @@
+// Type definitions for alt:V modules
+declare module 'alt-client' {
+    export * from '@altv/types-client';
+}
+
+declare module 'alt-server' {
+    export * from '@altv/types-server';
+}
+
+declare module 'natives' {
+    export * from '@altv/types-natives';
+}
+
+declare module 'alt-shared' {
+    export * from '@altv/types-shared';
+}
